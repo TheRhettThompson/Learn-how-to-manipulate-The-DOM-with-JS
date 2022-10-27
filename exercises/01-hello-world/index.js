9d94beafc0fcd8465da6eae6c1e24f1937fc2927
TheRhettThompson <rthompsoncareer@gmail.com>
2022-10-27 23:46:25 +0000

    added additional completed exercises

--- exercises/01-hello-world/index.js
+++ exercises/01-hello-world/index.js
@@ -1,4 +1,3 @@
 //alert here
-    alert! hello
+alert("Hello World")
 
-}
--- exercises/09-Render-on-Click/index.js
+++ exercises/09-Render-on-Click/index.js
@@ -2,4 +2,6 @@ let button = document.querySelector("#superDuperButton");
 button.addEventListener("click", function() {
 	//your code here
 	
+
+	
 });
